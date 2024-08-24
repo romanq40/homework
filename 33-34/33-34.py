eaf0c1158756d1efcf558a37b80098845e2a4c0d
romanq40 <romanq40@mail.ru>
2024-08-24 16:33:47 +0500

    33-34

---
+++ 33-34/33-34.py
@@ -0,0 +1,20 @@
+import json
+# a = {}
+# for i in range(3):
+#     name = input('Введите имя')
+#     age = int(input('Введите возраст'))
+#     a[name] = age
+# new_a = json.dumps(a, indent=2)
+# print(new_a)
+
+# with open('text3.json','w') as file:
+#     json.dump(a,file,indent=2)
+
+with open('text3.json','r') as file:
+    c = json.load(file)
+f = 0
+for value in c.values():
+    if value > f:
+        f=value
+print(f)
+
